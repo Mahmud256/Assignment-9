1a74b4c62cf7327e7f143692f459ec939f3d15ae
Mahmud256 <mahmud474849@gmail.com>
2023-10-09 19:31:40 +0600

    Include AOS Pakege

--- src/pages/Instructors/Instructor.jsx
+++ src/pages/Instructors/Instructor.jsx
@@ -14,7 +14,7 @@ const Instructor = ({}) => {
     }, [])
     return (
         <div>
-            <h1 className='text-3xl text-red-700 font-bold text-center pt-12'>Our Professional Instructors</h1>
+            <h1 className='text-3xl text-red-700 font-bold text-center pt-12' data-aos="fade-up">Our Professional Instructors</h1>
             <div className="flex justify-around py-12">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
                     {
