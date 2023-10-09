5920478d962ba783455e4de7aaf9e7730c281cb9
Mahmud256 <mahmud474849@gmail.com>
2023-10-09 20:58:02 +0600

    Subscribe Page is Update

--- src/pages/Subscribe/Subscribe.jsx
+++ src/pages/Subscribe/Subscribe.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Swal from 'sweetalert2';
+import { AuthContext } from '../../providers/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const Subscribe = () => {
+    const { user } = useContext(AuthContext); 
+
     const [email, setEmail] = useState('');
 
     const handleSubscribe = (e) => {
-        e.preventDefault(); // Prevent the default form submission
+        e.preventDefault(); 
 
-        // Check if email is empty
+        // Check if the user is not logged in
+        if (!user) {
+            
+            Swal.fire({
+                icon: 'info',
+                title: 'Please Log In',
+                text: 'You need to log in to subscribe.',
+            });
+            return;
+        }
+
+        
         if (!email) {
             Swal.fire({
                 icon: 'error',
@@ -17,35 +32,62 @@ const Subscribe = () => {
             return;
         }
 
-        // Display success SweetAlert
+        
         Swal.fire({
             icon: 'success',
             title: 'Subscribe Successful!',
             text: 'Your subscribe is done.',
         });
 
-        // Clear the email input field after successful subscription
+        
         setEmail('');
     };
 
     return (
-        <div className="form-control bg-slate-600">
-            <form onSubmit={handleSubscribe}>
-                <div className="input-group justify-center my-12">
-                    <input
-                        type="email"
-                        placeholder="email"
-                        name="email"
-                        className="input input-bordered"
-                        required
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <button type="submit" className="btn btn-error normal-case text-[#fff]">
-                        Subscribe
-                    </button>
+        <div>
+            {user ? (
+                <div className="form-control bg-slate-600">
+                    <form onSubmit={handleSubscribe}>
+                        <div className="input-group justify-center my-12">
+                            <input
+                                type={email}
+                                placeholder="email"
+                                name="email"
+                                className="input input-bordered"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <button type="submit" className="btn btn-error normal-case text-[#fff]">
+                                Subscribe
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            ) : (
+                <>
+                    <div className="form-control bg-slate-600">
+                        <form>
+                            <div className="input-group justify-center my-12">
+                                <input
+                                    type={email}
+                                    placeholder="email"
+                                    name="email"
+                                    className="input input-bordered"
+                                    required
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <Link to="/login" className="btn btn-error normal-case text-[#fff]">
+                                    Subscribe
+                                </Link>
+
+                            </div>
+                        </form>
+                    </div>
+
+                </>
+            )}
         </div>
     );
 };
